18f38d662575f2655e5779ff726f0321469eac32
Renee Yu <renyu4910@gmail.com>
2022-11-11 15:19:14 -0500

    Add automated code

--- software/car/car.ino
+++ software/car/car.ino
@@ -10,7 +10,18 @@ IMU imu;
 Ultrasonic ultrasonic;
 Motors motors;
 
+const int NUMTURNS = 11;
+const float MINDIST = 5;
+const float MAX_PITCH = 10;
+
+float distToTurn[NUMTURNS] = {13, 13, 13, 43, 43, 43, 43, 73, 73, 73, 73};
+
+float leftSpeed = 0.9, rightSpeed = 0.9, leftTurn = 0.9, rightTurn = -0.9, adjustment = 0.1;
+
 unsigned long lastSensorPrint = 0;
+float distanceToWall = 0;
+int usState = 0;
+float lastUSValue = 0;
 
 void setup()
 {
@@ -20,94 +31,132 @@ void setup()
     ultrasonic.initialize();
     motors.initialize();
     pinMode(START_BUTTON_PIN, INPUT_PULLUP);
-    // delay(3000);
-    // imu.setZeroes(true, false, false);
-    // turn();
+    Serial.println("Waiting for start button press");
+    while (digitalRead(START_BUTTON_PIN) == HIGH); // Wait until start button is pressed
+    Serial.println("Start button pressed");
+    delay(5000); // When start button is pressed, wait 5 secs before starting
+}
+
+void runMotors(float left, float right) {
+    // check direction
+    MotorDirection dirLeft = (left < 0 ? backward : forward);
+    MotorDirection dirRight = (right < 0 ? backward : forward);
+
+	//start left motor
+	motors.setMotorDirection(Motor(0), dirLeft);
+	motors.setMotorPower(Motor(0), abs(left));
+    
+	motors.setMotorDirection(Motor(1), dirLeft);
+	motors.setMotorPower(Motor(1), abs(left));
+	//start right motor
+	motors.setMotorDirection(Motor(2), dirRight);
+	motors.setMotorPower(Motor(2), abs(right));
+    
+	motors.setMotorDirection(Motor(3), dirRight);
+	motors.setMotorPower(Motor(3), abs(right));
+}
+
+void turn(int turnNum) {
+	if (ultrasonic.getDist() < distToTurn[turnNum]) {
+		//stop motors
+		runMotors(leftTurn, rightTurn);
+	}
+
+	float currentAngle = imu.getIMUData().yaw;
+	while (imu.getIMUData().yaw < (currentAngle + 90)) {
+		imu.updateIMUState();
+        if (millis() - lastSensorPrint > 1000) {
+            lastSensorPrint = millis();
+        }
+	}
+
+	runMotors(0, 0);
+
+}
+
+void adjustWheels(int val) {
+	if (val > 0) {
+		runMotors(leftSpeed, rightSpeed + adjustment);
+	}
+	else if (val < 0) {
+		runMotors(leftSpeed + adjustment, rightSpeed);
+	}
+}
+
+void checkDistance() {
+	if ((usState == 0) && ((lastUSValue - MINDIST) > distanceToWall)) {
+		usState = 1;
+		distanceToWall = lastUSValue;
+	} else if ((usState == 1) && ((lastUSValue - MINDIST) < distanceToWall)) {
+		usState = 0;
+		lastUSValue = ultrasonic.getDist();
+	}
+
+	if ((usState == 0) && ((lastUSValue - MINDIST) < distanceToWall)) {
+		lastUSValue = ultrasonic.getDist();
+	}
+
+	if ((usState == 1) && ((lastUSValue - MINDIST) > distanceToWall)) {
+		distanceToWall = lastUSValue;
+	}
 }
 
 void loop()
-{   
-
+{
     imu.updateIMUState();
     if (millis() - lastSensorPrint > 1000) {
         lastSensorPrint = millis();
+		lastUSValue = ultrasonic.getDist();
         printSensorData();
     }
 
-    // if (imu.getIMUData().yaw > 90 && imu.getIMUData().yaw < 180) {
-    //     stop();
-    // }
+	delay(10000);
 
-    processCommand();
+	imu.setZeroes(true, true, true);
+
+    for (int i = 0; i < NUMTURNS; i ++) {
+		distanceToWall = ultrasonic.getDist();
+		//start motors
+		runMotors(leftSpeed, rightSpeed);
+		while (distanceToWall > distToTurn[i]) {
+            imu.updateIMUState();
+            if (millis() - lastSensorPrint > 1000) {
+                lastSensorPrint = millis();
+            }
+			if (imu.getIMUData().pitch < abs(MAX_PITCH))
+				distanceToWall = ultrasonic.getDist();
+			//checkDistance();
+			//adjustThread.check();
+		}
+		turn(i);
+	}
 }
+
 
 void printSensorData() {
     Serial.println("Pitch: " + String(imu.getIMUData().pitch) + " Yaw: " + String(imu.getIMUData().yaw) + " Roll: " + String(imu.getIMUData().roll));
     Serial.println("Distance: " + String(ultrasonic.getDist()));
 }
 
-void processCommand() {
-    char command = Serial.read();
-    // q - forward max
-    // a - forward half
-    // w - backward max
-    // s - backward half
-    // x - stop
-
-    if (command != -1) {
-        Serial.println(command);
-        if (command == 'q') { 
-            motors.setMotorDirection(front_left, forward);
-            motors.setMotorDirection(front_right, backward);
-            motors.setMotorDirection(back_left, forward);
-            motors.setMotorDirection(back_right, backward);
-
-            for (int i = 0; i < NUM_MOTORS; i++) {
-                motors.setMotorPower(Motor(i), 1);
-            }
-        } else if (command == 'a') {
-            for (int i = 0; i < NUM_MOTORS; i++) {
-                motors.setMotorDirection(Motor(i), forward);
-                motors.setMotorPower(Motor(i), 1);
-            }
-        } else if (command == 'w') {
-            for (int i = 0; i < NUM_MOTORS; i++) {
-                motors.setMotorDirection(Motor(i), backward);
-                motors.setMotorPower(Motor(i), 1);
-            }
-        } else if (command == 's') {
-            for (int i = 0; i < NUM_MOTORS; i++) {
-                motors.setMotorDirection(Motor(i), backward);
-                motors.setMotorPower(Motor(i), 0.5);
-            }
-        } else if (command == 'x') {
-            for (int i = 0; i < NUM_MOTORS; i++) {
-                motors.setMotorPower(Motor(i), 0);
-            }
-        }
+/*
+void loop()
+{
+    imu.updateIMUState();
+    if (millis() - lastSensorPrint > 1000) {
+        lastSensorPrint = millis();
+		lastUSValue = ultrasonic.getDist();
+        printSensorData();
     }
-}
 
-void goForward() {
-            for (int i = 0; i < NUM_MOTORS; i++) {
-            motors.setMotorDirection(Motor(i), forward);
-            motors.setMotorPower(Motor(i), 0.5);
-        }
+    runMotors(leftSpeed, rightSpeed);
+	while (distanceToWall > distToTurn[i]) {
+            imu.updateIMUState();
+            if (millis() - lastSensorPrint > 1000) {
+                lastSensorPrint = millis();
+            }
+			distanceToWall = ultrasonic.getDist();
+			checkDistance();
+			//adjustThread.check();
+		}
 }
-
-void turn() {
-    motors.setMotorDirection(front_left, forward);
-    motors.setMotorDirection(front_right, backward);
-    motors.setMotorDirection(back_left, forward);
-    motors.setMotorDirection(back_right, backward);
-
-    for (int i = 0; i < NUM_MOTORS; i++) {
-        motors.setMotorPower(Motor(i), 1);
-    }
-}
-
-void stop() {
-            for (int i = 0; i < NUM_MOTORS; i++) {
-            motors.setMotorPower(Motor(i), 0);
-        }
-}
+*/
