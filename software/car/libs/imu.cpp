ca391602a6cbe6621f7bb1d9109b9d837b95b527
Chamath Wijesekera <chamath2.wijesekera@gmail.com>
2022-11-10 22:04:39 -0500

    Integrate DMP into IMU code

--- software/car/libs/imu.cpp
+++ software/car/libs/imu.cpp
@@ -2,9 +2,10 @@
 
 
 IMU::IMU() {
-    // Variables for AHRS loop timing
-    tau = 0.98;
-    data = {0,0,0};
+  MPU9250_DMP imu;
+  yaw_offset = 0;
+  pitch_offset = 0;
+  roll_offset = 0;
 }
 
 /**
@@ -13,12 +14,12 @@ IMU::IMU() {
  */
 void IMU::initialize() {
     while (!Serial); // wait for connection
-    // Initialize MPU9250 device
-    setup_mpu_6050_registers();
     // verify connection
-    Serial.println("MPU9250 OK");
-    loopTimer = micros();
-    loopTimer2 = micros();
+    Serial.println(imu.begin() == INV_SUCCESS ? "MPU9250 OK" : "MPU9250 ERROR");
+
+    imu.dmpBegin(DMP_FEATURE_6X_LP_QUAT | // Enable 6-axis quat
+              DMP_FEATURE_GYRO_CAL, // Use gyro calibration
+            10); // Set DMP FIFO rate to 10 Hz
 }
 
 /**
@@ -26,94 +27,48 @@ void IMU::initialize() {
  * 
  */
 void IMU::updateIMUState() {
-    freq = 1/((micros() - loopTimer2) * 1e-6);
-    loopTimer2 = micros();
-    dt = 1/freq;
-
-    // Read the raw acc data from MPU-6050
-    read_mpu_6050_data();
-
-    // Subtract the offset calibration value
-    gyro_x -= G_off[0];
-    gyro_y -= G_off[1];
-    gyro_z -= G_off[2];
-
-    // Convert to instantaneous degrees per second
-    rotation_x = (double)gyro_x / (double)scaleFactorGyro;
-    rotation_y = (double)gyro_y / (double)scaleFactorGyro;
-    rotation_z = (double)gyro_z / (double)scaleFactorGyro;
-
-    // Convert to g force
-    accel_x = (double)acc_x / (double)scaleFactorAccel;
-    accel_y = (double)acc_y / (double)scaleFactorAccel;
-    accel_z = (double)acc_z / (double)scaleFactorAccel;
-
-    // Complementary filter
-    accelPitch = atan2(accel_y, accel_z) * RAD_TO_DEG;
-    accelRoll = atan2(accel_x, accel_z) * RAD_TO_DEG;
-    accelYaw = atan2(accel_x, accel_y) * RAD_TO_DEG;
-
-    data.pitch = (tau)*(data.pitch + rotation_x*dt) + (1-tau)*(accelPitch);
-    data.roll = (tau)*(data.roll - rotation_y*dt) + (1-tau)*(accelRoll);
-    // data.yaw = (tau)*(data.yaw + rotation_z*dt) + (1-tau)*(accelYaw);
-    data.yaw = gyroYaw;
-
-    if (data.yaw < 0) data.yaw += 360.0;
-    if (data.yaw >= 360.0) data.yaw -= 360.0;
-
-    gyroPitch += rotation_x*dt;
-    gyroRoll -= rotation_y*dt;
-    gyroYaw += rotation_z*dt;
+  // Check for new data in the FIFO
+  if ( imu.fifoAvailable() )
+  {
+    // Use dmpUpdateFifo to update the ax, gx, mx, etc. values
+    if ( imu.dmpUpdateFifo() == INV_SUCCESS)
+    {
+      // computeEulerAngles can be used -- after updating the
+      // quaternion values -- to estimate roll, pitch, and yaw
+      imu.computeEulerAngles();
+    }
+  }
 }
 
-/**
- * @brief Gets IMU data
- * 
- * @return IMUData 
- */
+void IMU::setZeroes(bool yaw, bool pitch, bool roll) {
+  if (yaw) {
+    yaw_offset = -imu.yaw;
+  }
+
+  if (pitch) {
+    pitch_offset = -imu.pitch;
+  }
+
+  if (roll) {
+    roll_offset = -imu.roll;
+  }
+}
+
 IMUData IMU::getIMUData() {
-    return data;
+  float raw_yaw = imu.yaw;
+  float raw_pitch = imu.pitch;
+  float raw_roll = imu.roll;
+
+  float pitch = -(raw_pitch - 360); 
+  
+  // Process roll
+  float roll = raw_roll;
+  if (roll > 180) roll -= 360;
+
+  // Process pitch
+  pitch = raw_pitch - 360;
+
+  IMUData data = {imu.yaw, imu.pitch, imu.roll};
+  return data;
 }
-
-void IMU::setup_mpu_6050_registers() {
-  //Activate the MPU-6050
-  Wire.beginTransmission(0x68);
-  Wire.write(0x6B);
-  Wire.write(0x00);
-  Wire.endTransmission();
-
-  // Configure the accelerometer
-  // Wire.write(0x__);
-  // Wire.write; 2g --> 0x00, 4g --> 0x08, 8g --> 0x10, 16g --> 0x18
-  Wire.beginTransmission(0x68);
-  Wire.write(0x1C);
-  Wire.write(0x08);
-  Wire.endTransmission();
-
-  // Configure the gyro
-  // Wire.write(0x__);
-  // 250 deg/s --> 0x00, 500 deg/s --> 0x08, 1000 deg/s --> 0x10, 2000 deg/s --> 0x18
-  Wire.beginTransmission(0x68);
-  Wire.write(0x1B);
-  Wire.write(0x08);
-  Wire.endTransmission();
-}
-
-void IMU::read_mpu_6050_data() {
-  // Subroutine for reading the raw data
-  Wire.beginTransmission(0x68);
-  Wire.write(0x3B);
-  Wire.endTransmission();
-  Wire.requestFrom(0x68, 14);
-
-  // Read data --> Temperature falls between acc and gyro registers
-  acc_x = Wire.read() << 8 | Wire.read();
-  acc_y = Wire.read() << 8 | Wire.read();
-  acc_z = Wire.read() << 8 | Wire.read();
-  temperature = Wire.read() <<8 | Wire.read();
-  gyro_x = Wire.read()<<8 | Wire.read();
-  gyro_y = Wire.read()<<8 | Wire.read();
-  gyro_z = Wire.read()<<8 | Wire.read();
-}
-
 
--- software/car/libs/imu.h
+++ software/car/libs/imu.h
@@ -3,14 +3,11 @@
 
 #include <Arduino.h>
 #include <Wire.h>
-#include "imu/I2Cdev.cpp"
-#include "imu/MPU9250.cpp"
+#include <SparkFunMPU9250-DMP.h>
 
 ////////////
 // Config //
 ////////////
-float G_off[3] = {121.6, -71.0, 53.0}; //raw offsets, determined for gyro at rest
-
 struct IMUData{
     float yaw;
     float pitch;
@@ -19,34 +16,18 @@ struct IMUData{
 
 class IMU {
     private:
-        IMUData data = {0,0,0};  
-        long loopTimer, loopTimer2;
-        int temperature;
-        double accelRoll, accelPitch, accelYaw;
-        long acc_x, acc_y, acc_z;
-        double accel_x, accel_y, accel_z;
-        double gyroRoll, gyroPitch, gyroYaw;
-        int gyro_x, gyro_y, gyro_z;
-        long gyro_x_cal, gyro_y_cal, gyro_z_cal;
-        double rotation_x, rotation_y, rotation_z;
-        double freq, dt;
-        double tau;
-
-        // 250 deg/s --> 131.0, 500 deg/s --> 65.5, 1000 deg/s --> 32.8, 2000 deg/s --> 16.4
-        long scaleFactorGyro = 65.5;
-
-        // 2g --> 16384 , 4g --> 8192 , 8g --> 4096, 16g --> 2048
-        long scaleFactorAccel = 8192;
-
-        void setup_mpu_6050_registers();
-        void read_mpu_6050_data();
-    
+        MPU9250_DMP imu;
+        float yaw_offset;
+        float pitch_offset;
+        float roll_offset;
     public:
         IMU();
 
         void initialize();
 
         void updateIMUState();
+
+        void setZeroes(yaw, pitch, roll);
 
         IMUData getIMUData();
 };
